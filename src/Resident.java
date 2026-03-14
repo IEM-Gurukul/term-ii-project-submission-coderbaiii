b3adefff6b41f978e0088444b34d3162a2eba879
Baivabi Chakraborty <chakrabortybaivabi@gmail.com>
2026-03-14 19:47:38 +0530

    Added comments to Resident class

--- src/Resident.java
+++ src/Resident.java
@@ -2,17 +2,18 @@
  public class Resident {
 
     int id;
+    // Stores resident name
     String name;
     String contact;
     int apartmentNumber;
-
+// Constructor to initialize resident details
     Resident(int id, String name, String contact, int apartmentNumber) {
         this.id = id;
         this.name = name;
         this.contact = contact;
         this.apartmentNumber = apartmentNumber;
     }
-
+// Displays resident information
     void display() {
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
