a4a1369f147e8ef7b053754a9b471dd28644bca8
Baivabi Chakraborty <chakrabortybaivabi@gmail.com>
2026-03-14 19:38:57 +0530

    Added comments to Apartment class

--- src/Apartment.java
+++ src/Apartment.java
@@ -1,13 +1,15 @@
 public class Apartment {
-
+// Stores apartment number
     int apartmentNumber;
     boolean occupied;
+
+    // Constructor to initialize apartment data
 
     Apartment(int apartmentNumber) {
         this.apartmentNumber = apartmentNumber;
         this.occupied = false;
     }
-
+  // Displays apartment details
     void display() {
         System.out.println("Apartment Number: " + apartmentNumber);
         System.out.println("Occupied: " + occupied);
