850bde73c5a37c06a064f413b18aa01a9d910be1
Baivabi Chakraborty <chakrabortybaivabi@gmail.com>
2026-03-16 19:25:16 +0530

    Added comments to custom exception class

--- src/ResidentNotFoundException.java
+++ src/ResidentNotFoundException.java
@@ -1,5 +1,6 @@
+// Custom exception for resident not found errors
 public class ResidentNotFoundException extends Exception {
-
+    // Constructor to pass exception message
     public ResidentNotFoundException(String message) {
         super(message);
     }
