e34bffcc1825d90a079f4030815878e8bc5b100e
Baivabi Chakraborty <chakrabortybaivabi@gmail.com>
2026-03-16 19:13:44 +0530

    Added comments to Tenant class

--- src/Tenant.java
+++ src/Tenant.java
@@ -1,11 +1,15 @@
  public class Tenant extends Resident {
 
     int rent;
+        // Constructor to initialize tenant details
+
 
     Tenant(int id, String name, String contact, int apartmentNumber, int rent) {
         super(id, name, contact, apartmentNumber);
         this.rent = rent;
     }
+    // Displays tenant information
+
 
     void display() {
         super.display();
