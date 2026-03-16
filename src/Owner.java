c6c08849fe8ffe05e27a3c754a73e21e02dd9265
Baivabi Chakraborty <chakrabortybaivabi@gmail.com>
2026-03-16 19:08:43 +0530

    Added comments to Owner class

--- src/Owner.java
+++ src/Owner.java
@@ -1,11 +1,13 @@
 public class Owner extends Resident {
 
     boolean ownsApartment;
+        // Constructor to initialize owner details
 
     Owner(int id, String name, String contact, int apartmentNumber) {
         super(id, name, contact, apartmentNumber);
         this.ownsApartment = true;
     }
+    // Displays owner information
 
     void display() {
         super.display();
